d75b2711d28391a87b21b61c3fe84993d7dbb535
Ben Smith <benasmith278@gmail.com>
2023-03-06 23:19:05 -0500

    Sort ops and write to file

--- main.py
+++ main.py
@@ -4,32 +4,64 @@ log_string = ""
 times = []
 text = []
 ops = []
-first_ind = 0
 
-def dissect_string():
-    with open ('log.txt') as file:
+def dissect_string(filepath):
+    with open (filepath) as file:
         data = file.read().strip()
 
-        while len(data) > 500:
-            match = re.search(r" {3} ?[0-9]+\.[0-9]+]", data)
-            times.append(match.group()[:-1].strip())                            # add times string to list
+        while find_next_match(data, find_numbers(data).span()[1]):
+            match = find_numbers(data)                  
+            times.append(float(match.group()[:-1].strip()))                     # add times string to list
             first_ind = match.span()                                            # get indices of match
             
             next = data[first_ind[1]:]                                          # repeat for next match
-            next_match = re.search(r" {3} ?[0-9]+\.[0-9]+]", next)
+            next_match = find_numbers(next)
             next_ind = next_match.span()
             text.append(data[first_ind[1]+1:(next_ind[0]+first_ind[1]-2)])      # get text from between matches
 
             data = next
+        
+        last_match = find_numbers(data)
+        times.append(float(last_match.group()[:-1].strip()))
+        text.append(data[last_match.span()[1]:])
 
-    return data
+    return data                   
 
-def get_nums(sting):
-    return(re.search(r" {3} ?[0-9]+\.[0-9]+]", sting))                          # match the timestamp of bootlog
+def find_next_match(string, index):                                             # find next match starting at index
+    next = string[index:]
+    next_match = find_numbers(next)
 
-dissect_string()
+    return next_match
 
-for x in range(len(times)):
-    ops.append([times[x], text[x]])                                             # make [[time, text], ...]
+def find_numbers(string):
+    return re.search(r" {3} ?[0-9]+\.[0-9]+]", string)                          # match the timestamp of bootlog
 
-print(ops[25])
+def sort_func(e):
+    return e['elapsed']
+
+def get_times(filepath):
+    dissect_string(filepath)
+    out = open("output.txt", "w")
+
+    for x in range(len(times)):                                                 # make [{time: time, text: text, elapsed: elapsed}, ...]
+        ops.append({"time": times[x],
+                    "text": text[x],
+                    "elapsed": 0})
+    
+    for x in range(len(ops) - 1):                                               # add elapsed time
+        elapsed = times[x] - times[x - 1]
+        if elapsed < 0:
+            elapsed = 0
+        ops[x]["elapsed"] = elapsed
+    
+    ops[len(ops) - 1]['elapsed'] = times[len(ops) - 1] - times[len(ops) - 2]    # last op
+    ops.sort(key=sort_func, reverse=True)
+
+    for op in ops:
+        out.write("OPERATION: " + op["text"] +
+                  "\n TIME: " + str(op["elapsed"]) + 
+                  "\n\n")
+    
+    return
+
+get_times("log.txt")
